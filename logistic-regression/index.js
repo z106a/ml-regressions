12b0ad70e6d586163200585554d465cf79b2587c
Evgeny Volk <evgeny.volk@mysky.com>
2020-01-09 10:53:38 +0700

    first

---
+++ logistic-regression/index.js
@@ -0,0 +1,31 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('../load-csv');
+const LogisticRegression = require('./logistic-regression');
+
+const { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
+  dataColumns: [
+    'horsepower',
+    'displacement',
+    'Weight',
+  ],
+  labelColumns: ['passedemissions'],
+  shuffle: true,
+  splitTest: 50,
+  converters: {
+    passedemissions: (value) => (
+      value === 'TRUE' ? 1 : 0
+    ),
+  }
+});
+
+const regression = new LogisticRegression(features, labels, {
+  learningRate: 0.5,
+  iterations: 100,
+  batchSize: 50
+});
+
+regression.train();
+regression.predict([
+  [130, 307, 1.75]
+]).print();
